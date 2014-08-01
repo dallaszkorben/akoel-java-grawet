25e1d4ce6e8e9a19d226b7c53692fb03337f5ec1
dallaszkorben@gmail.com <dallaszkorben@gmail.com@ebc4fbb4-041a-0bdc-706d-1eedc26f7b23>
2014-08-01 00:14:15 +0000

    popup menu for tree

--- src/hu/akoel/grawit/tree/PageBaseTree.java
+++ src/hu/akoel/grawit/tree/PageBaseTree.java
@@ -1,6 +1,8 @@
 package hu.akoel.grawit.tree;
 
 import java.awt.Component;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import hu.akoel.grawit.CommonOperations;
 import hu.akoel.grawit.tree.tags.PageBaseTreeElement;
@@ -9,7 +11,10 @@ import hu.akoel.grawit.tree.tags.PageBaseTreePage;
 import hu.akoel.grawit.tree.tags.PageBaseTreeRoot;
 
 import javax.swing.ImageIcon;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JTree;
+import javax.swing.SwingUtilities;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 
@@ -53,5 +58,46 @@ public class PageBaseTree extends JTree{
 		    }
 		});
 		
+		this.addMouseListener( new RightMouseListener() );
+		
+	
 	}
+	
+	class RightMouseListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			
+						
+			
+			 if (SwingUtilities.isRightMouseButton(e)) {
+
+		            int row = PageBaseTree.this.getClosestRowForLocation(e.getX(), e.getY());
+		            PageBaseTree.this.setSelectionRow(row);
+		            
+		            JPopupMenu menu = new JPopupMenu ();
+                    menu.add ( new JMenuItem ( "Up" ) );
+                    menu.add ( new JMenuItem ( "Down" ) );
+		            
+		            menu.show(e.getComponent(), e.getX(), e.getY());
+		        }
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}	 
+	 }
 }
