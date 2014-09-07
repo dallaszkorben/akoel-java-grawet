32fcd068cf3170a627ecf2d21c3fa1be2c41b3ec
dallaszkorben@gmail.com <dallaszkorben@gmail.com@ebc4fbb4-041a-0bdc-706d-1eedc26f7b23>
2014-09-07 22:36:22 +0000

    Van egy kis hiba

--- src/hu/akoel/grawit/core/pages/torlendo_BasePageChangeListener.java
+++
@@ -1,8 +0,0 @@
-package hu.akoel.grawit.core.pages;
-
-import hu.akoel.grawit.core.treenodedatamodel.base.BaseElementDataModel;
-
-public interface torlendo_BasePageChangeListener {
-
-	public void removeBaseElement( BaseElementDataModel element );
-}
--- src/hu/akoel/grawit/core/pages/torlendo_CustomPageInterface.java
+++
@@ -1,9 +0,0 @@
-package hu.akoel.grawit.core.pages;
-
-import hu.akoel.grawit.ExecutablePageInterface;
-
-public interface torlendo_CustomPageInterface extends ExecutablePageInterface{
-	
-	public String getSurce();
-	
-}
