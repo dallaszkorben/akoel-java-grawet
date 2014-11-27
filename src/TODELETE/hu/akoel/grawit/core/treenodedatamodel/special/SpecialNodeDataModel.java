d74f0edcfa2fbbcbe85e335d2c21415cdf2669db
dallaszkorben@gmail.com <dallaszkorben@gmail.com@ebc4fbb4-041a-0bdc-706d-1eedc26f7b23>
2014-11-27 01:14:58 +0000

    9

---
+++ src/TODELETE/hu/akoel/grawit/core/treenodedatamodel/special/SpecialNodeDataModel.java
@@ -0,0 +1,180 @@
+package TODELETE.hu.akoel.grawit.core.treenodedatamodel.special;
+
+import java.util.Vector;
+
+import javax.swing.tree.MutableTreeNode;
+
+import hu.akoel.grawit.CommonOperations;
+import hu.akoel.grawit.core.treenodedatamodel.SpecialDataModelInterface;
+import hu.akoel.grawit.enums.Tag;
+import hu.akoel.grawit.exceptions.XMLMissingAttributePharseException;
+import hu.akoel.grawit.exceptions.XMLPharseException;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class SpecialNodeDataModel extends SpecialDataModelInterface{
+
+	private static final long serialVersionUID = -5125611897338677880L;
+	
+	public static final Tag TAG = Tag.SPECIALNODE;
+	
+	public static final String ATTR_DETAILS = "details";
+	
+	private String name;
+	private String details;
+	
+	public SpecialNodeDataModel( String name, String details ){
+		super( );
+		this.name = name;
+		this.details = details;
+	}
+	
+	/**
+	 * XML alapjan legyartja a BASENODE-ot es az alatta elofordulo 
+	 * BASENODE-okat, illetve BASEPAGE-eket
+	 * 
+	 * @param element
+	 * @throws XMLMissingAttributePharseException 
+	 */
+	public SpecialNodeDataModel( Element element ) throws XMLPharseException{
+		
+		if( !element.hasAttribute( ATTR_NAME ) ){
+			throw new XMLMissingAttributePharseException( SpecialNodeDataModel.getRootTag(), Tag.SPECIALNODE, ATTR_NAME );			
+		}
+		String nameString = element.getAttribute( ATTR_NAME );
+		this.name = nameString;
+		
+		if( !element.hasAttribute( ATTR_DETAILS ) ){
+			throw new XMLMissingAttributePharseException( SpecialNodeDataModel.getRootTag(), Tag.SPECIALNODE, ATTR_NAME, getName(), ATTR_DETAILS );			
+		}		
+		String detailsString = element.getAttribute( ATTR_DETAILS );		
+		this.details = detailsString;
+		
+		NodeList nodelist = element.getChildNodes();
+		for( int i = 0; i < nodelist.getLength(); i++ ){
+			Node node = nodelist.item( i );
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element baseElement = (Element)node;
+				
+				//Ha SPECIALOPEN van alatta
+				if( baseElement.getTagName().equals( Tag.SPECIALOPEN.getName() )){
+					this.add(new SpecialOpenDataModel(baseElement));
+				
+				//Ha SPECIALCLOSE van alatta
+				}else if( baseElement.getTagName().equals( Tag.SPECIALCLOSE.getName() )){
+					this.add(new SpecialCloseDataModel(baseElement));
+					
+				//Ha SPECIALCUSTOM van alatta
+				}else if( baseElement.getTagName().equals( Tag.SPECIALCUSTOM.getName() )){
+					this.add(new SpecialCustomDataModel(baseElement));
+						
+				//Ha ujabb BASENODE van alatta
+				}else if( baseElement.getTagName().equals( Tag.SPECIALNODE.getName() )){
+					this.add(new SpecialNodeDataModel(baseElement));
+				}
+			}
+		}
+	}
+	
+	public static Tag getTagStatic(){
+		return TAG;
+	}
+
+	@Override
+	public Tag getTag() {
+		return getTagStatic();
+	}
+
+	@Override
+	public void add(SpecialDataModelInterface node) {
+		super.add( (MutableTreeNode)node );
+	}
+	
+	public static String  getModelNameToShowStatic(){
+		return CommonOperations.getTranslation( "tree.nodetype.special.node");
+	}
+	
+	@Override
+	public String getNodeTypeToShow(){
+		return getModelNameToShowStatic();
+	}
+	
+	@Override
+	public String getName(){
+		return name;
+	}
+	
+	public String getDetails(){
+		return details;
+	}
+	
+	public void setDetails( String details ){
+		this.details = details;
+	}
+	
+	public void setName( String name ){
+		this.name = name;
+	}
+	
+	public String toString(){
+		return name;
+	}
+
+	@Override
+	public Element getXMLElement(Document document) {
+		Attr attr;
+		
+		//Node element
+		Element nodeElement = document.createElement( SpecialNodeDataModel.this.getTag().getName() );
+		attr = document.createAttribute( ATTR_NAME );
+		attr.setValue( getName() );
+		nodeElement.setAttributeNode(attr);	
+		
+		attr = document.createAttribute( ATTR_DETAILS );
+		attr.setValue( getDetails() );
+		nodeElement.setAttributeNode(attr);	
+	
+		int childrens = this.getChildCount();
+		for( int i = 0; i < childrens; i++ ){
+			
+			Object object = this.getChildAt( i );
+			
+			if( !object.equals(this) && object instanceof SpecialDataModelInterface ){
+				
+				Element element = ((SpecialDataModelInterface)object).getXMLElement( document );
+				nodeElement.appendChild( element );		    		
+		    	
+			}
+		}
+	
+		return nodeElement;		
+	}
+
+	@Override
+	public Object clone(){
+		
+		SpecialNodeDataModel cloned = (SpecialNodeDataModel)super.clone();
+	
+		if( null != this.children ){
+			cloned.children = (Vector<?>) this.children.clone();
+		}
+		
+		return cloned;
+		
+	}
+	
+	@Override
+	public Object cloneWithParent() {
+		
+		SpecialNodeDataModel cloned = (SpecialNodeDataModel) this.clone();
+		
+		//Le kell masolni a felmenoit is, egyebkent azok automatikusan null-ok
+		cloned.setParent( (MutableTreeNode) this.getParent() );
+		
+		return cloned;
+	}	
+}
---
+++ src/TODELETE/hu/akoel/grawit/gui/editor/special/SpecialCustomEditor.java
@@ -0,0 +1,208 @@
+package TODELETE.hu.akoel.grawit.gui.editor.special;
+
+import java.awt.Component;
+import java.text.MessageFormat;
+import java.util.LinkedHashMap;
+
+import hu.akoel.grawit.CommonOperations;
+import hu.akoel.grawit.gui.editor.DataEditor;
+import hu.akoel.grawit.gui.editors.component.ScriptComponent;
+import hu.akoel.grawit.gui.editors.component.TextFieldComponent;
+import hu.akoel.grawit.gui.tree.Tree;
+
+import javax.swing.JLabel;
+import javax.swing.tree.TreeNode;
+import javax.tools.JavaCompiler.CompilationTask;
+
+import TODELETE.hu.akoel.grawit.core.treenodedatamodel.special.SpecialCustomDataModel;
+import TODELETE.hu.akoel.grawit.core.treenodedatamodel.special.SpecialNodeDataModel;
+
+public class SpecialCustomEditor extends DataEditor{
+
+	private static final long serialVersionUID = 157539958460178584L;
+
+	private Tree tree; 
+	private SpecialCustomDataModel nodeForModify;
+	private SpecialNodeDataModel nodeForCapture;
+	private EditMode mode;
+	
+	private JLabel labelName;
+	private TextFieldComponent fieldName;
+	
+	private JLabel labelScript;
+	private ScriptComponent fieldScript;
+		
+	//Itt biztos beszuras van
+	public SpecialCustomEditor( Tree tree, SpecialNodeDataModel selectedNode ){
+
+		super( SpecialCustomDataModel.getModelNameToShowStatic());
+		
+		this.tree = tree;
+		this.nodeForCapture = selectedNode;
+		this.mode = null;
+		
+		//Name
+		fieldName = new TextFieldComponent( "" );
+		
+		common( "" );
+		
+	}
+	
+	//Itt lehet hogy modositas vagy megtekintes van
+	public SpecialCustomEditor( Tree tree, SpecialCustomDataModel selectedNode, EditMode mode ){
+
+		super( mode, selectedNode.getNodeTypeToShow());
+
+		this.tree = tree;
+		this.nodeForModify = selectedNode;
+		this.mode = mode;
+		
+		//Name		
+		fieldName = new TextFieldComponent( selectedNode.getName());
+		
+		common( selectedNode.getScript() );
+		
+	}
+	
+	private void common( String script ){
+		labelName = new JLabel( CommonOperations.getTranslation("editor.label.name") + ": ");
+		labelScript = new JLabel( CommonOperations.getTranslation("editor.label.special.script") + ": ");
+	
+		//Script
+		fieldScript = new ScriptComponent( SpecialCustomDataModel.getCodePre(), script, SpecialCustomDataModel.getCodePost() );	
+	
+		this.add( labelName, fieldName );
+		this.add( labelScript, fieldScript );
+
+	}
+	
+	@Override
+	public void save() {
+		
+		//Ertekek trimmelese
+		fieldName.setText( fieldName.getText().trim() );
+		
+		//
+		//Hibak eseten a hibas mezok osszegyujtese
+		//
+		LinkedHashMap<Component, String> errorList = new LinkedHashMap<Component, String>();		
+		if( fieldName.getText().length() == 0 ){
+			errorList.put( 
+					fieldName,
+					MessageFormat.format(
+							CommonOperations.getTranslation("editor.errormessage.emptyfield"), 
+							"'"+labelName.getText()+"'"
+					)
+			);
+				
+		}else if( fieldScript.getScript().length() == 0 ){
+			errorList.put( 
+					fieldScript,
+					MessageFormat.format(
+							CommonOperations.getTranslation("editor.errormessage.emptyfield"), 
+							"'"+labelScript.getText()+"'"
+					)
+			);
+				
+		}else{
+
+			TreeNode nodeForSearch = null;
+
+			//CAPTURE
+			if( null == mode){
+				
+				nodeForSearch = nodeForCapture;
+				
+			//MODIFY
+			}else if( mode.equals( EditMode.MODIFY )){
+				
+				nodeForSearch = nodeForModify.getParent();
+				
+			}
+			
+			//Megnezi, hogy van-e masik azonos nevu elem
+			int childrenCount = nodeForSearch.getChildCount();
+			for( int i = 0; i < childrenCount; i++ ){
+				TreeNode levelNode = nodeForSearch.getChildAt( i );
+				
+				//Ha Close-rol van szo (Lehetne meg Node/Page/Close is) TODO 
+				if( levelNode instanceof SpecialCustomDataModel ){
+					
+					//Ha azonos a nev
+					if( ((SpecialCustomDataModel) levelNode).getName().equals( fieldName.getText() ) ){
+					
+						//Ha rogzites van, vagy ha modositas, de a vizsgalt node kulonbozik a modositott-tol
+						if( null == mode || ( mode.equals( EditMode.MODIFY ) && !levelNode.equals(nodeForModify) ) ){
+										
+							errorList.put( 
+								fieldName, 
+								MessageFormat.format( 
+										CommonOperations.getTranslation("editor.errormessage.duplicateelement"), 
+										fieldName.getText(), 
+										CommonOperations.getTranslation("tree.nodetype.special.open") 
+								) 
+							);
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		//Volt hiba
+		if( errorList.size() != 0 ){
+			
+			//Hibajelzes
+			this.errorAt( errorList );
+		
+		//Ha nem volt hiba
+		}else{
+			
+			//Akkor eloszor a kod szintaktikai ellenorzese kovetkezik
+			SpecialCustomDataModel customDataModel = new SpecialCustomDataModel( fieldName.getText(), fieldScript.getScript() );				
+			
+			//Kod legyartasa
+			CompilationTask task = customDataModel.generateTheCode();
+			
+			//Kod forditasa
+			boolean success = customDataModel.compileTheCode( task );
+
+			//Ha NEM sikerult a forditas
+			if( !success ){
+				errorList.put( 
+						fieldScript, 
+						//MessageFormat.format( 
+								CommonOperations.getTranslation("editor.errormessage.formaterrorcustomscript") + "\n\n" + 
+								customDataModel.getDiagnostic()
+								/*, 
+								fieldScript.getScript(), 
+								CommonOperations.getTranslation("tree.nodetype.special.custom")*/ 
+						//) 
+				);
+			
+				//Hibajelzes
+				this.errorAt( errorList );
+			
+			//Hibatlan minden szempontbol
+			}else{			
+			
+				//Uj rogzites eseten
+				if( null == mode ){
+			
+					//SpecialCustomDataModel newSpecial = new SpecialCustomDataModel( fieldName.getText(), fieldScript.getText() );				
+					nodeForCapture.add( customDataModel );
+				
+					//Modositas eseten
+				}else if( mode.equals(EditMode.MODIFY ) ){
+				
+					nodeForModify.setName( fieldName.getText() );
+					nodeForModify.setScript( fieldScript.getScript() );
+			
+				}			
+			
+				//A fa-ban is modositja a nevet (ha az valtozott)
+				tree.changed();
+			}
+		}
+	}
+}
