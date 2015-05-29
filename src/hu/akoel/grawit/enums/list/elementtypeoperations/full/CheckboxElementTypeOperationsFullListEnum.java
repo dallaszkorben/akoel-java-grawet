b72b3650200166bcdec72e75d3322d34adce25a6
afoldvarszky <afoldvarszky@TEST-AFoldvarszky.statlogics.local>
2015-05-29 17:12:26 +0200

    Translation fix

--- src/hu/akoel/grawit/enums/list/elementtypeoperations/full/CheckboxElementTypeOperationsFullListEnum.java
+++ src/hu/akoel/grawit/enums/list/elementtypeoperations/full/CheckboxElementTypeOperationsFullListEnum.java
@@ -5,7 +5,7 @@ import hu.akoel.grawit.enums.list.elementtypeoperations.ElementTypeOperationsLis
 
 public enum CheckboxElementTypeOperationsFullListEnum implements ElementTypeOperationsListEnumInterface{		
 	CLICK( 0, CommonOperations.getTranslation( "editor.label.step.elementtype.checkbox.click") ),	
-	MOVE_TO_ELEMENT( 1, CommonOperations.getTranslation( "editor.label.step.elementtype.checkbox.movetoelment") ),
+	MOVE_TO_ELEMENT( 1, CommonOperations.getTranslation( "editor.label.step.elementtype.checkbox.movetoelement") ),
 	COMPAREVALUE_TO_CONSTANT( 2, CommonOperations.getTranslation( "editor.label.step.elementtype.checkbox.comparevaluetoconstant") ),
 	COMPAREVALUE_TO_STORED( 3, CommonOperations.getTranslation( "editor.label.step.elementtype.checkbox.comparevaluetostored") ),
 	COMPAREVALUE_TO_STRING( 4, CommonOperations.getTranslation( "editor.label.step.elementtype.checkbox.comparevaluetostring") ),
